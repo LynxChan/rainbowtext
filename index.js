ae7f9a6e73131cc3e7fd0132bce928189a6f8b28
LynxChan <69234828+LynxChan@users.noreply.github.com>
2020-08-05 08:49:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var common = require('../../engine/postingOps').common;
+
+exports.engineVersion = '2.4';
+
+exports.init = function() {
+
+  var originalProcessLine = common.processLine;
+
+  common.processLine = function(split, replaceCode) {
+
+    split = originalProcessLine(split, replaceCode);
+
+    split = split.replace(/##(.+?)##/g, "<span class=\"rainbow\">$1</span>");
+
+    return split;
+
+  };
+
+};
